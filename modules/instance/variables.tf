6c1ae6f0d05f3bd9c1ed6a3f30b14a9bb110b125
AKSHAY RATHEESH <92791421+AkshayRatheesh@users.noreply.github.com>
2022-12-20 18:56:49 +0530

    comltkj

--- modules/instance/variables.tf
+++ modules/instance/variables.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 variable "ami_id" {}
 variable "network_insterface_id" {}
 variable "instance_type" {
@@ -9,16 +8,4 @@ variable "availability_zone" {
 }
 variable "instance_count" {
   default = "1"
-=======
-variable "ami_id" {}
-variable "network_insterface_id" {}
-variable "instance_type" {
-  default = "t3.micro"
-}
-variable "availability_zone" {
-  default = "ap-south-2a"
-}
-variable "instance_count" {
-  default = "1"
->>>>>>> 99a943c881dbaf530f8a13ded9f558d2b0308759
 }
--- modules/security/variables.tf
+++ modules/security/variables.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 variable "vpc_id" {}
 
 variable "sg_name" {}
@@ -15,21 +14,3 @@ variable "security_groups_id" {}
 
 
 
-=======
-variable "vpc_id" {}
-
-variable "sg_name" {}
-
-variable "env_name" {}
-
-variable "sg_tag_name" {}
-
-variable "sg_description" { 
-    default = "ggngdnnghnhgn "
-}
-
-variable "security_groups_id" {}
-
-
-
->>>>>>> 99a943c881dbaf530f8a13ded9f558d2b0308759
